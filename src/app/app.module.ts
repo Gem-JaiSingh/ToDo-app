223581748554dcc40d07e49d7eb230377b67512a
riya1 <riya.rana@geminisolutions.com>
2022-06-23 09:05:37 +0530

    Todo app completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainWindowComponent } from './main-window/main-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWindowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-window/main-window.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../Model/task';
+
+@Component({
+  selector: 'app-main-window',
+  templateUrl: './main-window.component.html',
+  styleUrls: ['./main-window.component.css']
+})
+export class MainWindowComponent implements OnInit {
+
+  todos:Task[]=[];
+  
+  constructor() { }
+
+  addTaskValue : string = '';
+  
+
+  ngOnInit(): void {
+  }
+
+  addTask(){
+    this.todos.push({
+      taskname: this.addTaskValue
+      
+    });
+
+    this.addTaskValue = "";
+  }
+
+  deleteTask(i:number){
+    this.todos.splice(i,1)
+  }
+
+  editTask(i:number){
+    let title = this.todos[i].taskname;
+   let result = prompt("Edit Task Title", title);
+   if (result !== null && result !== ""){
+     this.todos[i].taskname = result;
+   }
+
+  }
+
+}
