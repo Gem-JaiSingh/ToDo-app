96e8d77ec0065b830290674c11ece0afa2b4232d
riya1 <riya.rana@geminisolutions.com>
2022-06-24 16:14:01 +0530

    Some update in edit mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,7 @@ import { TaskServiceService } from './task-service.service';
 // import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-// import { CarouselModule } from 'ngx-bootstrap/carousel';
-// import { CollapseModule } from 'ngx-bootstrap/collapse';
-// import { BsDatepickerModule, BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
-// import { BsDropdownModule,BsDropdownConfig } from 'ngx-bootstrap/dropdown';
-// import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
+
 
 
 @NgModule({
