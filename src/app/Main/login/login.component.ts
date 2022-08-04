488a49a14f32414114492a65f9e3f08ee9f252c0
riya1 <riya.rana@geminisolutions.com>
2022-08-04 15:46:16 +0530

    login form validation done

--- src/app/Main/login/login.component.ts
+++ src/app/Main/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public loginForm !: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      
+      
+      loginemail: new FormControl (
+          "",
+          [
+            Validators.required,
+            Validators.maxLength(60),
+            Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+          ]),
+          loginpassword: new FormControl (
+            "",
+            [
+              Validators.required,
+              Validators.minLength(8),
+              Validators.maxLength(20),
+              
+            ]),
+      
+      
+    });
   }
 
 }
